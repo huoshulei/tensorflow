904c0118ab49a23e974ca7e9f37dbebe96bdd07c
霍述雷 <hsl_4444@163.com>
2018-02-05 17:25:39 +0800

    rnn

--- tf/RNN分类.py
+++ tf/RNN分类.py
@@ -21,8 +21,8 @@ weights = {
 }
 
 biases = {
-    'in': tf.Variable(tf.constant(.1, shape=[n_hidden_units, ])),
-    'out': tf.Variable(tf.constant(.1, shape=[n_classes, ]))
+    'in': tf.Variable(tf.constant(.1, shape=[n_hidden_units])),
+    'out': tf.Variable(tf.constant(.1, shape=[n_classes]))
 }
 
 
@@ -31,34 +31,33 @@ def RNN(x, w, b):
     # ==>{128*28,28]
     x = tf.reshape(x, [-1, n_inputs])
     # ==>[128*28,128]
-    x_in = tf.matmul(x, weights['in']) + biases['in']
+    x_in = tf.matmul(x, w['in']) + b['in']
     # ==>[128,28,128]
     x_in = tf.reshape(x_in, [-1, n_steps, n_hidden_units])
-
     lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1., state_is_tuple=True)
     # lstm cell is divided into two pars(c_state,m_state)
     _init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
     outputs, states = tf.nn.dynamic_rnn(lstm_cell, x_in, initial_state=_init_state, time_major=False)
 
-    # results = tf.matmul(states[1], weights['out']) + biases['out']
+    results = tf.matmul(states[1], w['out']) + b['out']
     # or
-    outputs = tf.unstack(tf.transpose(outputs, [1, 0, 2]))
-    results = tf.matmul(outputs[-1], weights['out']) + biases['out']
+    # outputs = tf.unstack(tf.transpose(outputs, [1, 0, 2]))
+    # results = tf.matmul(outputs[-1], weights['out']) + biases['out']
     return results
 
 
 prediction = RNN(x, weights, biases)
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=y))
 train = tf.train.AdamOptimizer(lr).minimize(loss)
-correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+correct_prod = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prod, tf.float32))
 
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
     step = 0
     while step * batch_size < training_iters:
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        batch_x = batch_x.reshape([batch_size, n_steps, n_inputs])
+        batch_x, batch_y = mnist.train.next_batch(batch_size)  # batch_x shape=128*[28,28] batch_y shape=128*[10]
+        batch_x = batch_x.reshape([batch_size, n_steps, n_inputs])  # 1*[128, 28, 28]
         sess.run(train, feed_dict={x: batch_x, y: batch_y})
         if not step % 20:
             print(sess.run(accuracy, feed_dict={x: batch_x, y: batch_y}))
