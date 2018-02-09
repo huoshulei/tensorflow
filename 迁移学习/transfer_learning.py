adacd5bbe3a6816b8e207e1adc79e4b6a7671833
huoshulei <hsl_4444@163.com>
2018-02-09 23:10:23 +0800

    vgg

--- 迁移学习/transfer_learning.py
+++ 迁移学习/transfer_learning.py
@@ -4,4 +4,13 @@ transfer_learning
 拿不到数据
 娶不到参数
 等待梯子
+家里不需梯子就可以下载
 """
+import os
+from urllib.request import urlretrieve
+import numpy as np
+import tensorflow as tf
+import skimage.io
+import skimage.transform
+import matplotlib.pyplot as plt
+
