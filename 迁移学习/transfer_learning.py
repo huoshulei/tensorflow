f78ea305049e6ac5006d904157aaa59a6bec065f
huoshulei <hsl_4444@163.com>
2018-02-10 20:06:05 +0800

    ss

--- 迁移学习/transfer_learning.py
+++ 迁移学习/transfer_learning.py
@@ -14,3 +14,22 @@ import skimage.io
 import skimage.transform
 import matplotlib.pyplot as plt
 
+
+def download():
+    categories = ['tiger', 'kittycat']
+    for category in categories:
+        os.makedirs('./for_transfer_learning/data/{}'.format(category), exist_ok=True)
+        with open('./imagenet_{}'.format(category), 'r') as file:
+            urls = file.readlines()
+            n_urls = len(urls)
+            for i, url in enumerate(urls):
+                try:
+                    urlretrieve(url.strip(),
+                                './for_transfer_learning/data/{}/{}'.format(category, url.strip().split('/')[-1]))
+                    print('{} {}/{}'.format(url.strip(), i, n_urls))
+                except:
+                    print('{} {}/{}'.format(url.strip(), i, n_urls), '图片加载错误！')
+
+
+if __name__ == '__main__':
+    download()
